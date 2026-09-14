8363c454745714da423f49a30969ab92e74f5f56
Antony Botello Herrera <antonybotello@gmail.com>
2026-09-14 10:19:03 -0500

    feat: add Django project settings and initialize usuarios app

--- app/settings.py
+++ app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9=%(k0^@7)0u(&5gk-ji=c6a#el0(n)z_wcszymlvz6c_5qf24
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
