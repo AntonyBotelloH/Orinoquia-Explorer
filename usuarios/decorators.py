272be8ed6040b945bb38d2e31baac8654ad851c9
Antony Botello Herrera <antonybotello@gmail.com>
2026-09-21 12:00:32 -0500

    feat: add role-required decorator, admin base template, and user listing view with template

--- usuarios/decorators.py
+++ usuarios/decorators.py
@@ -7,7 +7,7 @@ def rol_requerido(*roles_permitidos):
     def decorator(view_func):
         @wraps(view_func)
         def wrapper(request, *args, **kwargs):
-            if request.user.is_authenticated and request.user.rol in roles_permitidos:
+            if request.user.is_authenticated and (request.user.is_superuser or request.user.rol in roles_permitidos):
                 return view_func(request, *args, **kwargs)
             else:
                 messages.error(request, "No tienes permisos para acceder a esta página")
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from .decorators import rol_requerido
+from .models import Usuario
 # Create your views here.
+
 
 @rol_requerido('ADMIN', 'GUIA')
 def listar_usuarios(request):
+    usuarios = Usuario.objects.all()
     context = {
-        "titulo": "Listar Usuarios"
+        "titulo": "Listar Usuarios",
+        "usuarios": usuarios
     }
     return render(request, "listar_usuarios.html", context)
