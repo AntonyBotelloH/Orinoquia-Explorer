a41721106dde45d479f01b3f40c1f95b788e0e9c
Antony Botello Herrera <antonybotello@gmail.com>
2026-09-24 10:50:15 -0500

    Fix logo and complete Experiencias and Reservas

--- reservas/urls.py
+++ reservas/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
-from .views import listar_reserva, crear_reserva, editar_reserva, eliminar_reserva
+from .views import listar_reserva, crear_reserva, editar_reserva, eliminar_reserva, listar_poliza, crear_poliza, editar_poliza, eliminar_poliza
 
 urlpatterns = [
     path('reserva/', listar_reserva, name='reserva_listar'),
     path('reserva/crear/', crear_reserva, name='reserva_crear'),
     path('reserva/editar/<int:id>/', editar_reserva, name='reserva_editar'),
     path('reserva/eliminar/<int:id>/', eliminar_reserva, name='reserva_eliminar'),
+
+    path('poliza/', listar_poliza, name='poliza_listar'),
+    path('poliza/crear/', crear_poliza, name='poliza_crear'),
+    path('poliza/editar/<int:id>/', editar_poliza, name='poliza_editar'),
+    path('poliza/eliminar/<int:id>/', eliminar_poliza, name='poliza_eliminar'),
 ]
--- reservas/views.py
+++ reservas/views.py
@@ -68,3 +68,67 @@ def eliminar_reserva(request, id):
         objeto.save()
         messages.success(request, 'Reserva cancelada correctamente.')
     return redirect('reserva_listar')
+
+@rol_requerido('ADMIN', 'GUIA')
+def listar_poliza(request):
+    polizas = PolizaSeguro.objects.all().order_by('fecha_expiracion')
+    titulo = 'Pólizas de Seguro'
+    context = {
+        'polizas': polizas,
+        'titulo': titulo
+    }
+    return render(request, 'reservas/listar_polizas.html', context)
+
+@rol_requerido('ADMIN')
+def crear_poliza(request):
+    accion = 'Crear'
+    titulo = 'Póliza de Seguro'
+    form = PolizaSeguroForm()
+    if request.method == 'POST':
+        form = PolizaSeguroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Póliza creada correctamente.')
+            return redirect('poliza_listar')
+        else:
+            for campo, errores in form.errors.items():
+                for error in errores:
+                    messages.error(request, f"Error en el campo '{campo.capitalize()}': {error}")
+    context = {
+        'titulo': titulo,
+        'form': form,
+        'accion': accion,
+    }
+    return render(request, 'partials/base-creacion.html', context)
+
+@rol_requerido('ADMIN')
+def editar_poliza(request, id):
+    poliza = get_object_or_404(PolizaSeguro, id=id)
+    accion = 'Editar'
+    titulo = 'Póliza de Seguro'
+    form = PolizaSeguroForm(instance=poliza)
+    if request.method == 'POST':
+        form = PolizaSeguroForm(request.POST, instance=poliza)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Póliza actualizada correctamente.')
+            return redirect('poliza_listar')
+        else:
+            for campo, errores in form.errors.items():
+                for error in errores:
+                    messages.error(request, f"Error en el campo '{campo.capitalize()}': {error}")
+
+    context = {
+        'titulo': titulo,
+        'form': form,
+        'accion': accion,
+    }
+    return render(request, 'partials/base-creacion.html', context)
+
+@rol_requerido('ADMIN')
+def eliminar_poliza(request, id):
+    objeto = get_object_or_404(PolizaSeguro, id=id)
+    if request.method == 'POST':
+        objeto.delete()
+        messages.success(request, 'Póliza eliminada correctamente.')
+    return redirect('poliza_listar')
